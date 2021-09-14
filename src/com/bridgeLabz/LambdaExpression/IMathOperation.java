4aec2855335878d89eea533c459f003059c9fbe1
ajaybarath <ajaybarath3@gmail.com>
2021-09-14 15:16:48 +0530

    ADDED: printResult function to print and calculate the function

--- src/com/bridgeLabz/LambdaExpression/IMathOperation.java
+++ src/com/bridgeLabz/LambdaExpression/IMathOperation.java
@@ -3,6 +3,10 @@ package com.bridgeLabz.LambdaExpression;
 interface IMathFunction {
 
 	int calculate(int a, int b);
+
+	public static void printResult(int a, int b, String function, IMathFunction obj) {
+		System.out.println("Result of " + function + " is : " + obj.calculate(a, b));
+	}
 }
 
 public class IMathOperation {
@@ -12,13 +16,14 @@ public class IMathOperation {
 		IMathFunction add = Integer::sum;
 
 		IMathFunction multiply = (x, y) -> x * y;
-		
+
 		IMathFunction divide = (int x, int y) -> x / y;
 
-		System.out.println("Addition is " + add.calculate(5,  3));
-		System.out.println("Multiplication is " + multiply.calculate(5,  3));
-		System.out.println("Division is " + divide.calculate(9,  3));
+		IMathFunction.printResult(4, 2, "Add", add);
 		
+		IMathFunction.printResult(4, 2, "Multiply", multiply);
+
+		IMathFunction.printResult(4, 2, "Divide", divide);
 
 	}
 
