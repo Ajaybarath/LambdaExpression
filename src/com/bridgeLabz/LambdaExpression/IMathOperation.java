a15be7407fc7f9939211d96e8bbfa9d8bd55e789
ajaybarath <ajaybarath3@gmail.com>
2021-09-14 13:29:22 +0530

    ADDED: lambda expression for add multiply and divission

--- src/com/bridgeLabz/LambdaExpression/IMathOperation.java
+++ src/com/bridgeLabz/LambdaExpression/IMathOperation.java
@@ -2,11 +2,24 @@ package com.bridgeLabz.LambdaExpression;
 
 interface IMathFunction {
 
-	int calculate(int a , int b);
+	int calculate(int a, int b);
 }
 
 public class IMathOperation {
-	
-	
+
+	public static void main(String args[]) {
+
+		IMathFunction add = Integer::sum;
+
+		IMathFunction multiply = (x, y) -> x * y;
+		
+		IMathFunction divide = (int x, int y) -> x / y;
+
+		System.out.println("Addition is " + add.calculate(5,  3));
+		System.out.println("Multiplication is " + multiply.calculate(5,  3));
+		System.out.println("Division is " + divide.calculate(9,  3));
+		
+
+	}
 
 }
